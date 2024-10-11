3880a25d4cb4e7ba60d975ae1fc1caeb2c580dbc
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2024-10-11 08:52:26 +0100

    FEAT (state): adds state logic to store activeSprint data
    Dynamically generates the activeSprint from Date.now() or passed Date
    
    Prepared to create the sprint dynamically
    
    Use persistent storage to make it a PWA
    
    Adds date-fns package

---
+++ src/lib/shared/stores/activeSprint.ts
@@ -0,0 +1,55 @@
+import { browser } from '$app/environment';
+
+import { writable } from 'svelte/store';
+
+import { getISOWeek, previousMonday } from "date-fns";
+
+export type SprintDay = {
+    complete: boolean,
+    date: Date,
+    week: number
+}
+
+const stateLabel = "activeSprint"
+
+const defaultValue = makeDefaultSprint(14);
+
+let initialValue = defaultValue;
+
+if (browser) {
+    const localActiveSprint = window.localStorage.getItem(stateLabel)
+
+    if (!localActiveSprint) {
+        initialValue = defaultValue
+    } else {
+        initialValue = JSON.parse(localActiveSprint);
+    }
+
+
+}
+
+const activeSprint = writable<SprintDay[]>(initialValue);
+
+activeSprint.subscribe((value) => {
+    if (browser) {
+        window.localStorage.setItem(stateLabel, JSON.stringify(value));
+
+    }
+});
+
+
+function makeDefaultSprint(length: number = 14, start: Date = new Date(Date.now())): SprintDay[] {
+    const startWeek: number = getISOWeek(start);
+    const sprintStart: Date = previousMonday(start);
+    return Array.from(Array(length)).map((day: SprintDay, index) => {
+        const datePointer = new Date(sprintStart)
+        datePointer.setDate(datePointer.getDate() + index)
+        return ({
+            complete: false,
+            date: datePointer,
+            week: index < 6 ? startWeek : startWeek + 1
+        }) as SprintDay
+    })
+}
+
+export default activeSprint;
